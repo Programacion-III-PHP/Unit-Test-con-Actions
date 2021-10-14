c3565bdfa43a76ee38b6cefbc952a53b8cafe4f0
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2021-10-14 19:10:03 -0300

    Rompemos todo

--- modelos/UsuarioModelo.class.php
+++ modelos/UsuarioModelo.class.php
@@ -9,10 +9,11 @@
             if ($this -> nombre === "usuario1" && $this -> password === "1234")
                 $this -> autenticado = true;
             else 
-                throw new Exception("Auth failed");               
+                throw new Exception("Auth failed")              
         }
 
         public function listar(){
+            var_dump(true);
             return array(
                 "id" => 1,
                 "nombre" => "pepito"
